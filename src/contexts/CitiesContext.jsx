b82a82607620175292aaae74e6e551cd71ad3fcd
Pramod Yadav <pramodyadavpy2711@gmail.com>
2024-03-17 13:00:59 +0530

    used Context API and a custom hook fot managing state

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const BASE_URL = 'http://localhost:9000';
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsloading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert('error loading data');
+      } finally {
+        setIsloading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error(' CitiesContext was used Outside the CitiesProvider');
+  return context;
+}
+
+export { CitiesProvider, useCities };
